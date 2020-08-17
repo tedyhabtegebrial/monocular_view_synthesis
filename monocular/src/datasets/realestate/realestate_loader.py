cee40db26c240bbea5076c781b08452a3392bee2
Ahmed Anwar <anwar@pc-2118.kl.dfki.de>
2020-08-17 14:16:32 +0200

    fix realestate loader

--- monocular/src/datasets/realestate/realestate_loader.py
+++ monocular/src/datasets/realestate/realestate_loader.py
@@ -7,9 +7,10 @@ class RealEstateLoader(Dataset):
 		self.configs = configs
 		self.dir = self.configs['dataset_root'] #/home5/anwar/data/realestate10k/
 		self.mode = configs['mode']
-		self.indices = np.loadtxt(os.path.join(self.dir, 'valid_folders_%s.txt')) %self.mode
-		self.frames = sorted(glob.glob(os.path.join(self.dir, 'extracted', self.mode, '*/*.jpg')))
-		self.text = sorted(glob.glob(os.path.join(self.dir, 'text_files', self.mode, '*/*.jpg')))
+		self.indices = np.loadtxt(os.path.join(self.dir, 'valid_folders_%s.txt' %self.mode) , dtype=np.str)
+		self.indices = self.indices.reshape((-1,))
+		self.frames = sorted([glob.glob(os.path.join(self.dir, 'extracted', self.mode, id, '*.jpg'))] for id in self.indices)
+		self.text = sorted([glob.glob(os.path.join(self.dir, 'text_files', self.mode, id, '*.txt'))] for id in self.indices)
 		# self.frames = [os.path.join(self.dir, 'extracted', self.mode, clip, frame) for clip in self.indices] 
 		# self.text = [os.path.join(self.dir, 'text_files', self.mode, clip, frame) for clip in self.indices]
 
@@ -26,7 +27,8 @@ class RealEstateLoader(Dataset):
 
 if __name__ == '__main__':
 	configs = {}
-	configs['dataset_root'] = '/home5/anwar/data/realestate10k'
+	configs['dataset_root'] = '/home/anwar/Desktop/realestate10k'
 	configs['mode'] = 'train'
 	
 	dataset = RealEstateLoader(configs)
+	print(len(dataset))
