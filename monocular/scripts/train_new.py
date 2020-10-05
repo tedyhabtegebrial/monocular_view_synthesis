fix color problem normalization bug

monocular/scripts/train_new.py
@@ -36,7 +36,7 @@ if is_teddy:
     configs['logging_dir'] = '/habtegebrialdata/monocular_nvs/experiment_logs/exp_1_with_bn_new_alpha_comp'
 else:
     configs['dataset_root'] = '/home5/anwar/data/realestate10k'
-    configs['logging_dir'] = '/home5/anwar/data/experiments/exp_SV'
+    configs['logging_dir'] = '/home5/anwar/data/experiments/exp_SV_GAN_LOSS_1'
 
 configs['mode'] = 'train'
 configs['max_baseline'] = 26
@@ -93,38 +93,39 @@ for epoch in range(configs['num_epochs']):
         gen_losses = trainer(data, mode='generator')[0]
 #        torch.cuda.synchronize()
 #        print('time 1', time.time() - start)
-#        gen_l = sum([v for k,v in gen_losses.items()]).mean()
-        gen_l = gen_losses['Total Loss'] # + gen_losses['GAN'] * gan_opts.lamda_gan
+        gen_l = sum([v for k,v in gen_losses.items()]).mean()
+        # gen_l = gen_losses['Total Loss']  + gen_losses['GAN'] * gan_opts.lamda_gan
         print('gen_l', gen_l.item())
         gen_l.backward()
         gen_optimizer.step()
         gen_optimizer.zero_grad()
 #        torch.cuda.synchronize()
 #        start = time.time()
-#        disc_losses = trainer(data, mode='discriminator')
+        disc_losses = trainer(data, mode='discriminator')
 #        torch.cuda.synchronize()
-#        print('time 2', time.time() - start)
-#        disc_l = sum([v for k,v in disc_losses.items()]).mean()
-#        disc_l.backward()
-#        disc_optimizer.step()
-#        disc_optimizer.zero_grad()
+       # print('time 2', time.time() - start)
+        disc_l = sum([v for k,v in disc_losses.items()]).mean()
+        disc_l.backward()
+        disc_optimizer.step()
+        disc_optimizer.zero_grad()
         novel_view = trainer.fake
         gen_print = {k:v.item() for k,v in gen_losses.items()}
-#        disc_print = {k:v.item() for k,v in disc_losses.items()}
+        disc_print = {k:v.item() for k,v in disc_losses.items()}
         print(f'epoch {epoch} iteration {itr}     generator  loss {gen_print}')
-#        print(f'epoch {epoch} iteration {itr}  discriminator loss {disc_print}')
+        print(f'epoch {epoch} iteration {itr}  discriminator loss {disc_print}')
         if(steps % 300 == 0):
-            novel_view = novel_view.data[:, [2,1,0], :, :].cpu()
-            target = a + (data['target_img'].data[:, [2,1,0], :, :].cpu() - min_val) * (b - a)/(max_val - min_val)
-            input_img = a + (data['input_img'].data[:, [2,1,0], :, :].cpu() - min_val) * (b - a)/(max_val - min_val)
+            novel_view = novel_view.data.cpu()
+            target = data['target_img'].data.cpu()
+            input_img = data['input_img'].data.cpu()
             torchvision.utils.save_image(novel_view, os.path.join(configs['logging_dir'], str(steps) +'_novel.png'))
-            writer.add_image('Novel View', novel_view[0], steps)
+            # writer.add_image('Novel View', novel_view[0], steps)
             #writer.add_scalar('Scalar', steps, steps)
             torchvision.utils.save_image(target, os.path.join(configs['logging_dir'], str(steps) +'_target.png'))
-            writer.add_image('Target View', target[0], steps)
+            # writer.add_image('Target View', target[0], steps)
             torchvision.utils.save_image(input_img, os.path.join(configs['logging_dir'], str(steps) +'_input.png'))
-            writer.add_image('Input View', input_img[0], steps)
+            # writer.add_image('Input View', input_img[0], steps)
         steps += 1
+        # exit()
 
     #writer.export_scalars_to_json(os.path.join(tb_path,'all_scalars.json')
     writer.close()

monocular/src/datasets/realestate/realestate_loader.py
@@ -8,6 +8,7 @@ import torch
 from PIL import Image
 from torchvision.transforms import Compose, ToTensor, Resize
 from math import sqrt
+import torchvision
 
 seed = 42
 class RealEstateLoader(Dataset):
@@ -19,7 +20,7 @@ class RealEstateLoader(Dataset):
 		self.indices = self.indices.reshape((-1,))
 		#print('Paths', glob.glob(os.path.join(self.dir, 'extracted', self.mode, self.indices[0], '*.jpg')))
 		#print('Indices', len(self.indices))
-		self.indices = self.indices[:5]
+		#self.indices = self.indices[:1]
 		self.frames = [glob.glob(os.path.join(self.dir, 'extracted', self.mode, id + '.txt', '*.jpg')) for id in self.indices]
 		#self.text = [glob.glob(os.path.join(self.dir, 'text_files', self.mode, id + '.txt')) for id in self.indices]
 		#print('ff', len(self.frames))
@@ -35,6 +36,7 @@ class RealEstateLoader(Dataset):
 		# self.text = [os.path.join(self.dir, 'text_files', self.mode, clip, frame) for clip in self.indices]
 
 		self.min_angle = 5
+		self.max_angle = 30
 		self.min_trans = 0.15
 		self.rng = np.random.RandomState(seed)
 
@@ -127,7 +129,7 @@ class RealEstateLoader(Dataset):
 		angles = np.array(angles)
 		translations = np.array(translations)
 
-		mask = target_candidates[(angles > self.min_angle) | (translations > self.min_trans)]
+		mask = target_candidates[(angles < self.max_angle) & (translations > self.min_trans)]
 
 		if(mask.shape[0] > 2):
 			target_idx = mask[self.rng.randint(mask.shape[0])]
@@ -187,13 +189,18 @@ if __name__ == '__main__':
 	configs['dataset_root'] = '/home5/anwar/data/realestate10k/'
 	configs['mode'] = 'train'
 	configs['max_baseline'] = 5
-	configs['height'] = 10
-	configs['width'] = 10
+	configs['height'] = 300
+	configs['width'] = 200
 
 	dataset = RealEstateLoader(configs)
 	print(len(dataset))
 	data = dataset.__getitem__(1)
 	print(data['input_img'].shape)
+	c, h, w = data['input_img'].shape
+	torchvision.utils.save_image(data['input_img'].permute([1,2,0]).reshape((c,h,w)), 'permute.png')
+	torchvision.utils.save_image(data['input_img'].reshape((h,w,c)).permute([2,0,1]), 'reshape.png')
+
+
 	# for itr, data in enumerate(dataset):
 	# 	print(data['input_img'].shape)
 	# 	break

monocular/src/models/single_view_network.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 class SingleViewNetwork(nn.Module):
 	def w_init(self, m):
@@ -9,8 +10,24 @@ class SingleViewNetwork(nn.Module):
 		m.bias = nn.init.zeros_(m.bias)
 		return m
 
+	def apply_harmonic_bias(self, channels, num_layers):
+		# Channels shape is [B,H,W,C]
+		# num_layers is 32
+		alpha = 1.0 / torch.range(2, num_layers, dtype=torch.float32)
+		#print('alpha shape', alpha.shape)
+		#print('channels shape', channels.shape)
+		shift = torch.Tensor([math.atanh(2.0 * x - 1.0) for x in alpha]).to(channels.device)
+		#shift = torch.atanh(2.0 * alpha - 1.0)
+		#print('shift shape', shift.shape)
+		no_shift = torch.zeros(channels.shape[-1] - num_layers + 1).to(channels.device)
+		#print('no shift shape', no_shift.shape)
+		shift = torch.cat([shift, no_shift], axis=-1)
+		# print(channels.shape)
+		return channels + shift
+
 	def __init__(self, configs):
 		super(SingleViewNetwork, self).__init__()
+		self.configs = configs
 
 		self.conv_1_0 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=7, padding=3)
 		self.conv_1_1 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=7, padding=3)
@@ -19,10 +36,10 @@ class SingleViewNetwork(nn.Module):
 		self.conv_2_1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=2)
 		self.bn_2 = nn.BatchNorm2d(64)
 		self.conv_3_0 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
-		self.conv_3_1 = nn.Conv2d(in_channels=128, out_channels=118, kernel_size=3, padding=1)
-		self.bn_3 = nn.BatchNorm2d(118)
+		self.conv_3_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+		self.bn_3 = nn.BatchNorm2d(128)
 
-		self.conv_4_0 = nn.Conv2d(in_channels=118, out_channels=256, kernel_size=3, padding=1)
+		self.conv_4_0 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
 		self.conv_4_1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
 		self.bn_4 = nn.BatchNorm2d(256)
 
@@ -58,7 +75,7 @@ class SingleViewNetwork(nn.Module):
 		self.conv_12_1 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
 		self.bn_12 = nn.BatchNorm2d(512)
 
-		self.conv_13_0 = nn.Conv2d(in_channels=512 + 118, out_channels=128, kernel_size=3, padding=1)
+		self.conv_13_0 = nn.Conv2d(in_channels=512 + 128, out_channels=128, kernel_size=3, padding=1)
 		self.conv_13_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
 		self.bn_13 = nn.BatchNorm2d(128)
 
@@ -74,44 +91,8 @@ class SingleViewNetwork(nn.Module):
 		self.conv_16_1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
 		self.bn_16 = nn.BatchNorm2d(64)
 		# self.batch16 = nn.BatchNorm2d(64)
-		self.output = nn.Conv2d(in_channels=64, out_channels=(configs['num_planes'] - 1) * 2 + 3, kernel_size=3, padding=1)
+		self.output = nn.Conv2d(in_channels=64, out_channels=configs['num_planes'] + 2, kernel_size=3, padding=1)
 
-		# self.output = nn.Conv2d(in_channels=64, out_channels=(configs['num_planes'] - 1) + 3, kernel_size=3, padding=1)
-		# self.output_1 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
-		# self.output_2 = nn.Conv2d(in_channels=32, out_channels=configs['num_planes'] * 2 + 2, kernel_size=3, padding=1)
-		# self.conv_1_0 = self.w_init(self.conv_1_0)
-		# self.conv_1_1 = self.w_init(self.conv_1_1)
-		# self.conv_2_0 = self.w_init(self.conv_2_0)
-		# self.conv_2_1 = self.w_init(self.conv_2_1)
-		# self.conv_3_0 = self.w_init(self.conv_3_0)
-		# self.conv_3_1 = self.w_init(self.conv_3_1)
-		# self.conv_4_0 = self.w_init(self.conv_4_0)
-		# self.conv_4_1 = self.w_init(self.conv_4_1)
-		# self.conv_5_0 = self.w_init(self.conv_5_0)
-		# self.conv_5_1 = self.w_init(self.conv_5_1)
-		# self.conv_6_0 = self.w_init(self.conv_6_0)
-		# self.conv_6_1 = self.w_init(self.conv_6_1)
-		# self.conv_7_0 = self.w_init(self.conv_7_0)
-		# self.conv_7_1 = self.w_init(self.conv_7_1)
-		# self.conv_8_0 = self.w_init(self.conv_8_0)
-		# self.conv_8_1 = self.w_init(self.conv_8_1)
-		# self.conv_9_0 = self.w_init(self.conv_9_0)
-		# self.conv_9_1 = self.w_init(self.conv_9_1)
-		# self.conv_10_0 = self.w_init(self.conv_10_0)
-		# self.conv_10_1 = self.w_init(self.conv_10_1)
-		# self.conv_11_0 = self.w_init(self.conv_11_0)
-		# self.conv_11_1 = self.w_init(self.conv_11_1)
-		# self.conv_12_0 = self.w_init(self.conv_12_0)
-		# self.conv_12_1 = self.w_init(self.conv_12_1)
-		# self.conv_13_0 = self.w_init(self.conv_13_0)
-		# self.conv_13_1 = self.w_init(self.conv_13_1)
-		# self.conv_14_0 = self.w_init(self.conv_14_0)
-		# self.conv_14_1 = self.w_init(self.conv_14_1)
-		# self.conv_15_0 = self.w_init(self.conv_15_0)
-		# self.conv_15_1 = self.w_init(self.conv_15_1)
-		# self.conv_16_0 = self.w_init(self.conv_16_0)
-		# self.conv_16_1 = self.w_init(self.conv_16_1)
-		# self.output = self.w_init(self.output)
 
 	def forward(self, rgb):
 		# Encoding
@@ -154,10 +135,32 @@ class SingleViewNetwork(nn.Module):
 		conv15 = F.relu((self.conv_15_1(F.relu(self.conv_15_0(torch.cat([up_14, conv1], dim=1))))))
 
 		conv16 = F.relu((self.conv_16_1(F.relu(self.conv_16_0(conv15)))))
-		conv16 = self.bn_16(conv16)
+		# conv16 = self.bn_16(conv16)
 
-		# output_1 = F.relu(self.output_1(conv16))
 		output = self.output(conv16)
-		print('alpha values:', output[:, :31, :, :].min().item(), output[:, :31, :, :].max().item())
-		# return F.sigmoid(output.clamp(min=-50.0, max=50.0))
-		return torch.sigmoid(output)
+		# b, c, h, w = output.shape
+		output = output.permute([0,2,3,1])
+		output = self.apply_harmonic_bias(output, self.configs['num_planes'])
+		# output = output.permute([0,3,1,2])
+		# print('alpha values:', output[:, :self.configs['num_planes'], :, :].min().item(), output[:, :self.configs['num_planes'], :, :].max().item())
+		# return torch.sigmoid(output)
+		return (torch.tanh(output) + 1.0) / 2.0
+
+
+if __name__ == '__main__':
+	configs = {}
+	configs['width'] = 256
+	configs['height'] = 256
+	configs['batch_size'] = 1
+	configs['num_planes'] = 64
+	configs['near_plane'] = 5
+	configs['far_plane'] = 10000
+	configs['encoder_features'] = 32
+	configs['encoder_ouput_features'] = 64
+	configs['input_channels'] = 3
+	configs['out_put_channels'] = 3
+	configs['occlusion_levels'] = 3
+	network = SingleViewNetwork_DFKI(configs).eval()
+	input_img = torch.rand(1, 3, 256, 256)
+	alphas_assoc = network(input_img)
+	print(f'Alphas and Association maps shape == {alphas_assoc.shape}')

monocular/src/monocular_stereo_magnification.py
@@ -13,8 +13,8 @@ from .mpi import ApplyHomography
 from .mpi import AlphaComposition
 from .mpi import ComputeBlendingWeights
 from .mpi import WarpWithFlowFields
-from .models import BackgroundNetwork, SingleViewNetwork_DFKI, ConvNetwork
-
+from .models import BackgroundNetwork, SingleViewNetwork_DFKI, ConvNetwork, SingleViewNetwork
+import torchvision
 class StereoMagnification(nn.Module):
     '''This is a class that can perform monocular view synthesis. Given a reference color image, camera intrinsics and extrinsics it returns a novel view.
     :param configs: a python dictionary that contains necessary configurations
@@ -25,12 +25,12 @@ class StereoMagnification(nn.Module):
         super(StereoMagnification, self).__init__()
         self.configs = configs
 
-        self.mpi_net = SingleViewNetwork_DFKI(configs)
+        self.mpi_net = SingleViewNetwork(configs)
         self.background = BackgroundNetwork(configs)
         self.reduce_high_features = BackgroundNetwork(configs, reduce=True)
 
 #        self.mpi_net = ConvNetwork(configs)
-        self.ComputeBlendingWeights = ComputeBlendingWeights()
+        self.compute_blending_weights = ComputeBlendingWeights()
         self.compute_homography = ComputeHomography(configs)
         self.apply_homography = ApplyHomography()
         self.composite = AlphaComposition()
@@ -68,26 +68,39 @@ class StereoMagnification(nn.Module):
     #     :return pred_img: novel view color image
     #     :return alphas:   per-plane alphas, returned so that we can see what the scene representation looks like
         '''
+        # output of net is [B,H,W,C]
         mpi_alphas_bg_img = self.mpi_net(input_img)
-        alpha = mpi_alphas_bg_img.permute([0,3,1,2])[:, :-3,...].unsqueeze(-1)
-        layer_alpha = torch.cat([torch.ones_like(alpha[:, 0:1]), alpha], axis=1)
-
-        fg_img = input_img.permute([0,2,3,1]).unsqueeze(1)
-        bg_img = mpi_alphas_bg_img[...,-3:].unsqueeze(1)
-
-        flipped_alphas = torch.flip(layer_alpha, dims=[1])
-        blending_weights = torch.cumprod(1.0 - flipped_alphas, axis = 1) / flipped_alphas
+        # b, h, w, c = mpi_alphas_bg_img.shape
+        alpha = mpi_alphas_bg_img.permute([0,3,1,2])[:,:-3,...].unsqueeze(-1)
+        layer_alpha = torch.cat([torch.ones_like(alpha[:,0:1]), alpha], axis=1)
+        # b, c, h, w = input_img.shape
+        fg_img = input_img.unsqueeze(1)
+        bg_img = mpi_alphas_bg_img[:,:,:,-3:].permute([0,3,1,2]).unsqueeze(1)
+        # create blending weight from alpha values by exclusive reversed cumulative product
+        # blending weights shape is [B, D, 1, H, W]
+        blending_weights = self.compute_blending_weights(layer_alpha)
+        # print('blending_weights', blending_weights.shape)
+        # print('fg_img', fg_img.shape)
+        # print('bg_img', bg_img.shape)
+        # flipped_alphas = torch.flip(layer_alpha, dims=[1])
+        # ones_ = torch.ones_like(flipped_alphas)[:,:1,...]
+        # blending_weights = torch.cumprod(1.0 - flipped_alphas, axis = 1)[:,:-1,...]
+        # blending_weights = torch.cat([ones_, blending_weights], axis=1)
+        # blending_weights = torch.flip(blending_weights, dims=[1])
 
         layer_rgb = blending_weights * fg_img + (1.0 - blending_weights) * bg_img
-        layers = torch.cat([layer_rgb, layer_alpha], axis = -1)
+        # layers = torch.cat([layer_rgb, layer_alpha], axis = -1)
         h_mats = self.compute_homography(kmats, rmats, tvecs)
 
+        # b, l, h, w, c = layer_alpha.shape
         layer_alpha = layer_alpha.permute([0,1,4,2,3])
-        layer_rgb = layer_rgb.permute([0,1,4,2,3])
+        # b, l, h, w, c = layer_rgb.shape
+        # layer_rgb = layer_rgb.permute([0,1,4,2,3])
 
         #print('layer alpha', layer_alpha.shape)
         #print('layer rgb', layer_rgb.shape)
         rgb_img, alphas = self._render_rgb(h_mats, layer_alpha, layer_rgb)
+
         if self.training:
             return rgb_img, alphas
         else:

monocular/src/mpi/alpha_composition.py
@@ -26,10 +26,18 @@ class AlphaComposition(nn.Module):
         alpha_imgs = torch.split(alpha_imgs, split_size_or_sections=1, dim=1)
         comp_rgb = src_imgs[-1] * alpha_imgs[-1]
         for d in reversed(range(num_d - 1)):
-            print('src_img', src_imgs[d].min(), src_imgs[d].max())
-            print('alpha imgs', alpha_imgs[d].min(), alpha_imgs[d].max())
-            print('comp_rgb', comp_rgb.min(), comp_rgb.max())
+            # print('comp_rgb', comp_rgb.min(), comp_rgb.max())
+            # print('src_img', src_imgs[d].min(), src_imgs[d].max())
+            # print('alpha imgs', alpha_imgs[d].min(), alpha_imgs[d].max())
+            # src_nan = torch.isnan(src_imgs[d])
+            # print(src_nan.shape)
+            # print('nan src',len(src_nan[src_nan == True]))
+            # alpha_nan = torch.isnan(alpha_imgs[d])
+            # print(alpha_nan.shape)
+            # print('nan alpha',len(alpha_nan[alpha_nan == True]))
+            # print(alpha_imgs[d])
             comp_rgb = src_imgs[d] * alpha_imgs[d] + (1.0 - alpha_imgs[d]) * comp_rgb
+            # print('comp_rgb', comp_rgb.min(), comp_rgb.max())
         return comp_rgb.squeeze(1)
 
 # class AlphaComposition(nn.Module):